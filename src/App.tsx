24043fd39b08409cdb00c7fa8a67eebb0243c6e4
Titus210 <tituskiplagat210@gmail.com>
2023-10-12 00:48:30 +0300

    Refactor: Update App Component Structure
    - The App component serves as the main entry point of the application.
    - It now renders the UserRoutes component for user-specific navigation.
    - The branding section, which includes the application name "Farmsight," is currently commented out but can be used for future branding.
    
    Fix Done: Refactored the App component for improved structure and routing.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import UserRoutes from "./routes";
+
+const App = () => {
+  return (
+    <>
+      {/* <div className="bg-white">
+        <h1 className="text-6xl font-mono font-bold text-green">Farm<span className="text-brown">sight.</span> </h1>
+        <Routes />
+      </div> */}
+      <UserRoutes />
+    </>
+  );
+}
+
+export default App;
